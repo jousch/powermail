7ba3551b7caa635daf14ce432f12f4e5793f1095
pbergsmann <p.bergsmann@opendo.at>
2013-05-08 15:18:22 +0200

    [BUGFIX] Replaces call to GeneralUtility::readLLXMLfile

--- pi1/class.tx_powermail_pi1_wizicon.php
+++ pi1/class.tx_powermail_pi1_wizicon.php
@@ -47,7 +47,7 @@ class tx_powermail_pi1_wizicon {
 
 	/**
 	 * Processing the wizard items array
-	 * 
+	 *
 	 * @param	array		$wizardItems: The wizard items
 	 * @return	array		array with wizard items
 	 */
@@ -65,18 +65,18 @@ class tx_powermail_pi1_wizicon {
 				'CType' => 'powermail_pi1',
 			),
 		);
-		
+
 		return $wizardItems;
 	}
 
 	/**
 	 * Reads the [extDir]/locallang.xml and returns the \$LOCAL_LANG array found in that file.
-	 * 
+	 *
 	 * @return	The array with language labels
 	 */
 	function includeLocalLang()	{
 		$llFile = t3lib_extMgm::extPath(powermail) . 'locallang.xml';
-		$LOCAL_LANG = t3lib_div::readLLXMLfile($llFile, $GLOBALS['LANG']->lang);
+		$LOCAL_LANG =  t3lib_l10n_parser_Llxml::getParsedData($llFile, $GLOBALS['LANG']->lang);
 
 		return $LOCAL_LANG;
 	}
